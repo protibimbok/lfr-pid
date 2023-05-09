dda24edf424cbe69438e7944c5c8f8fab2cdf811
Saad <sakib.saad.khan@gmail.com>
2023-05-09 16:18:08 +0600

    ignore unused var warning just for EEPROM.h

--- include/storage.h
+++ include/storage.h
@@ -1,9 +1,21 @@
 #ifndef LOADED_STORAGE_CTRL
 #define LOADED_STORAGE_CTRL
 
+/**
+ * This part of the code is not a necessary one.
+ * it just supresses the warning for unused include 'EEPROM'.
+ * but clearly we are using it.
+ * 
+ * I don't like unnecessary warnings, so until I figure out
+ * why this warning is happening - 
+ * "Enjoy your time in the darkness dear warning!"
+ */
+#pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-variable"
 
 #include <EEPROM.h>
+
+#pragma GCC diagnostic pop
 
 void write_ints(int* vals, int size);
 void read_ints(int* vals, int size);
