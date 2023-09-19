532e8796fafe0116d85b56eec455601628c5c396
Saad <sakib.saad.khan@gmail.com>
2023-09-19 17:25:01 +0600

    initial threshhold = 0

--- src/config.cpp
+++ src/config.cpp
@@ -2,7 +2,7 @@
 
 const int SIZE = 7;
 const int SENSORS[SIZE]            = {  A7,  A6,  A5,  A4,  A3,  A2,  A1 }; // A1,  A2,  A3,  A4,  A5,  A6,  A7
-int       THRESHOLDS[SIZE]         = { 500, 500, 500, 500, 500, 500, 500 }; // Sensor threshold values
+int       THRESHOLDS[SIZE]         = { 0,     0,   0,   0,   0,   0,   0 }; // Sensor threshold values
 int       READINGS[SIZE]           = { 0,     0,   0,   0,   0,   0,   0 }; // Sensor readings
 int       READINGS_BIN[SIZE]       = { 0,     0,   0,   0,   0,   0,   0 }; // Sensor readings in binary
 
